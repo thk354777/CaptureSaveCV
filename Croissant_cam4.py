e71c33ff6ea5ed8e52f5a348ed3c8803901ffb47
Theeradon Kaewnok <57024582+thk354777@users.noreply.github.com>
2023-09-21 22:32:33 +0700

    Croissant_cam4B.py
    video bigger

--- Croissant_cam4.py
+++ Croissant_cam4.py
@@ -22,7 +22,7 @@ class VideoCaptureThread(QThread):
         super().__init__()
         self.capture = cv2.VideoCapture(0)
         self.running = True
-        self.model = YOLO('best_2.pt')  # Initialize your YOLO model here
+        self.model = YOLO('best2.14.pt')  # Initialize your YOLO model here
 
     def run(self):
         while self.running:
@@ -34,15 +34,15 @@ class VideoCaptureThread(QThread):
             frame_with_objects = self.detect_objects(frame)
 
             # Convert OpenCV BGR image to QImage
-            height, width, channel = frame_with_objects.shape
+            height, width, channel = frame_with_objects[0].shape
             bytes_per_line = 3 * width
-            q_image = QImage(frame_with_objects.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+            q_image = QImage(frame_with_objects[0].data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
 
             self.new_frame.emit(q_image)
 
     def detect_objects(self, frame):
         # Perform object detection using your YOLO model here
-        results = self.model.predict(frame, conf=0.6, show=True)
+        results = self.model.predict(frame, conf=0.6, show=False)
 
         obj_lists = self.model.names  # Model Classes {0: 'cookie', 1: 'croissant', 2: 'donut'}
 
@@ -73,7 +73,42 @@ class VideoCaptureThread(QThread):
             frame_with_objects = cv2.putText(frame_with_objects, text, coordinates, font, fontScale, color, thickness, cv2.LINE_AA)
             coor_y += 50
 
-        return frame_with_objects
+        return frame_with_objects, obj_lists_count
+    
+    def detect_objects(self, frame):
+        # Perform object detection using your YOLO model here
+        results = self.model.predict(frame, conf=0.6, show=False)
+
+        obj_lists = self.model.names  # Model Classes {0: 'cookie', 1: 'croissant', 2: 'donut'}
+
+        objs = results[0].boxes.numpy()  # Arrays of Predicted result
+        obj_count = {value: key for key, value in obj_lists.items()}
+        obj_lists_count = dict.fromkeys({value: key for key, value in obj_lists.items()}, 0)
+
+        if objs.shape[0] != 0:  # Check if object > 0 piece.
+            for obj in objs:
+                detected_obj = obj_lists[int(obj.cls[0])]  # Change Object index to name.
+                obj_lists_count[detected_obj] += 1
+
+        # Draw bounding boxes and labels on the frame
+        frame_with_objects = frame.copy()
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.5
+        color = (255, 255, 0)
+        thickness = 2
+        coor_x = coor_y = 50
+
+        for bread, value in obj_lists_count.items():
+            if value > 1:
+                text = f'{value} {bread}s'
+            else:
+                text = f'{value} {bread}'
+
+            coordinates = (coor_x, coor_y)
+            frame_with_objects = cv2.putText(frame_with_objects, text, coordinates, font, fontScale, color, thickness, cv2.LINE_AA)
+            coor_y += 50
+
+        return frame_with_objects, obj_lists_count
 
     def stop(self):
         self.running = False
@@ -83,7 +118,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Video Capture and Save")
-        self.setGeometry(50, 50, 400, 300)
+        self.setGeometry(25, 25, 800, 600)
 
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
@@ -91,7 +126,8 @@ class MainWindow(QMainWindow):
         self.layout = QVBoxLayout(self.central_widget)
 
         self.video_label = QLabel(self)
-        self.layout.addWidget(self.video_label, alignment=Qt.AlignCenter)
+        self.video_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)  # Align the video to the left and top
+        self.layout.addWidget(self.video_label)
 
         self.capture_button = QPushButton("Capture Image", self)
         self.save_button = QPushButton("Save Image", self)
@@ -117,15 +153,23 @@ class MainWindow(QMainWindow):
     def update_video_label(self, frame):
         self.latest_frame = frame
         pixmap = QPixmap.fromImage(frame)
-        self.video_label.setPixmap(pixmap)
+
+        # Calculate the size for displaying the video with the desired width
+        desired_width = 900  # Change this to your desired width
+        scaled_pixmap = pixmap.scaledToWidth(desired_width, Qt.SmoothTransformation)
+
+        self.video_label.setPixmap(scaled_pixmap)
+
+    
 
     @Slot()
     def capture_image(self):
         if self.latest_frame is not None:
             self.captured_frame = self.latest_frame
-            pixmap = QPixmap.fromImage(self.captured_frame).scaled(320, 240, Qt.KeepAspectRatio)
+            pixmap = QPixmap.fromImage(self.captured_frame).scaled(32, 24, Qt.KeepAspectRatio)
             self.captured_label.setPixmap(pixmap)
-
+            print(self.video_thread.detect_objects(self.captured_frame)[1])
+            
     @Slot()
     def save_image(self):
         if self.captured_frame is not None:
